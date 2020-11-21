a6a00f14286476ca6d2197dda1350b5927a9c568
adam-maj <mr.adam.maj@gmail.com>
2020-11-21 17:13:07 -0500

    Admin and auth working, routes no longer all redirect to react app

--- backend/urls.py
+++ backend/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
+    path('<str:path>', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     path('authentication/', include('authentication.urls')),
-    path('<str:path>', TemplateView.as_view(template_name='index.html')),
-    path('', TemplateView.as_view(template_name='index.html'))
 ]
